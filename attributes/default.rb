47c38353535e1a359eae3f92df01e1be4abb5f52
Vincent Theron <vptheron@gmail.com>
2014-05-21 13:25:38 -0400

    Update default version

--- attributes/default.rb
+++ attributes/default.rb
@@ -19,7 +19,7 @@
 # limitations under the License.
 #
 
-default['idea']['version'] = '13.0.2'
+default['idea']['version'] = '13.1.2'
 default['idea']['64bits']['Xmx'] = '2048m'
 default['idea']['64bits']['Xms'] = '2048m'
 
