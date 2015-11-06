a7905245a648bbe6fc4fd8e727a7950c66ccf0aa
Vincent Theron <vptheron@gmail.com>
2015-11-06 15:53:17 -0500

    change default version

--- attributes/default.rb
+++ attributes/default.rb
@@ -19,7 +19,7 @@
 # limitations under the License.
 #
 
-default['idea']['version'] = '14.1.3'
+default['idea']['version'] = '14.1.4'
 default['idea']['edition'] = 'C'
 default['idea']['64bits']['Xmx'] = '2048m'
 default['idea']['64bits']['Xms'] = '2048m'
