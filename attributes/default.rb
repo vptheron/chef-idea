0c42a292e5bac704baf00d91363df23cb1905ad0
Chris Gianelloni <chris@cask.co>
2016-10-07 12:08:44 -0400

    Update default version to latest

--- attributes/default.rb
+++ attributes/default.rb
@@ -19,7 +19,7 @@
 # limitations under the License.
 #
 
-default['idea']['version'] = '2016.2.3'
+default['idea']['version'] = '2016.2.4'
 default['idea']['edition'] = 'C'
 default['idea']['64bits']['Xmx'] = '2048m'
 default['idea']['64bits']['Xms'] = '2048m'
